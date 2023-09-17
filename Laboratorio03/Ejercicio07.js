8425ac2565243018b33df5b8a498ed0bbc948587
MindOverMatterl <albert.vargas@tecsup.edu.pe>
2023-09-17 11:17:42 -0500

    Laboratorio03

---
+++ Laboratorio03/Ejercicio07.js
@@ -0,0 +1,18 @@
+let a = 3, b=2;
+let z= a + b;
+console.log("Resultado de la Suma: " + z);
+
+z= a-b;
+console.log("Resultado de la Resta: "+ z);
+
+z = a*b;
+console.log("Resultado del producto: "+ z);
+
+z= a/b;
+console.log("Resultado de la division: "+ z);
+
+z = a%b;
+console.log("Resultado de la operación modulo: " + z);
+
+z = a**b;
+console.log("Resultado del operador exponente: "+ z);
---
+++ Laboratorio03/Ejercicio24.js
@@ -0,0 +1,6 @@
+myFunction(4,2);
+function myFunction(a,b){
+    console.log("Suma " + (a+b)); 
+}
+
+myFunction(2,3);
